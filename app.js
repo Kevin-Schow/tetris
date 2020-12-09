291bd581df71a4ad95058d04f9e633a4346a69f1
Kevin Schow <Kevinschow@gmail.com>
2020-12-09 12:26:13 -0800

    working tetris blocks

--- app.js
+++ app.js
@@ -41,6 +41,27 @@ document.addEventListener('DOMContentLoaded', () => {
 	[width, width+1, width+2, width+3]
 	]
 
+	const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+	let currentPosition = 4
+	let currentRotation = 0
+	let random = Math.floor(Math.random()*theTetrominoes.length)
+	let current = theTetrominoes[random][currentRotation]
+
+	// Draw the tetromino
+	function draw() {
+		current.forEach(index => {
+			squares[currentPosition + index].classList.add('tetromino')
+		})
+	}
+
+	function undraw() {
+		current.forEach(index => {
+			squares[currentPosition + index].classList.remove('tetromino')
+		})
+	}
+	draw()
+
 
 
 })
